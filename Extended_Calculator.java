00eac0b3f9ce88e29005ba87a6affb2fa72818c6
01fe22bca160 <01fe22bca160@kletech.ac.in>
2024-02-20 09:56:38 +0530

    updated code

---
+++ Extended_Calculator.java
@@ -0,0 +1,28 @@
+
+/**
+ * The "demo" class extends the "Calculator" class and contains methods to calculate the square, cube,
+ * and modulus of two floating-point numbers.
+ */
+class Extended_Calculator {
+    /**
+     * The function takes a float operand and returns the square of that operand.
+     * 
+     * @param operand_2 The parameter "operand_2" is a float value that represents the number that we
+     * want to square.
+     * @return the square of the operand_2 value.
+     */
+    float square(float operand_2){
+          return(operand_2*operand_2);
+     }
+   /**
+    * The function cube takes a float operand_2 and returns the cube of that number.
+    * 
+    * @param operand_2 A float value that represents the number to be cubed.
+    * @return The cube of the operand_2 value.
+    */
+    float cube(float operand_2){
+          return(operand_2*operand_2*operand_2);
+     }
+    
+
+}
